28a25d1304de4dbaabfada67fffede638e951082
Felipe Baravieira <fe.belineb@gmail.com>
2017-03-01 20:45:27 -0300

    +decorator

---
+++ src/structural/decorator/decorator.js
@@ -0,0 +1,36 @@
+function Pasta() {
+    this.price = 0;
+}
+Pasta.prototype.getPrice = function() {
+    return this.price;
+}
+
+function Penne() {
+    this.price = 8;
+}
+Penne.prototype = Object.create(Pasta.prototype);
+
+
+function SauceDecorator(pasta) {
+    this.pasta = pasta;
+}
+
+SauceDecorator.prototype.getPrice = function() {
+    return this.pasta.getPrice() + 5;
+}
+
+function CheeseDecorator(pasta) {
+    this.pasta = pasta;
+}
+
+CheeseDecorator.prototype.getPrice = function() {
+    return this.pasta.getPrice() + 3;
+}
+
+module.exports = [Penne, SauceDecorator, CheeseDecorator];
+
+var penne = new Penne();
+var penneWithSauce = new SauceDecorator(penne);
+var penneWithSauceAndCheese = new CheeseDecorator(penneWithSauce);
+console.log(penneWithSauceAndCheese.getPrice());
+
---
+++ test/decorator-test.js
@@ -0,0 +1,18 @@
+const expect = require('chai').expect;
+const [Penne, SauceDecorator, CheeseDecorator] = require('../src/structural/decorator/decorator');
+
+
+
+describe('decorator tests', () => {
+
+    it('sanity test', () => {
+        var penne = new Penne();
+        var penneWithSauce = new SauceDecorator(penne);
+        var penneWithSauceAndCheese = new CheeseDecorator(penneWithSauce);
+
+        expect(penne.getPrice()).to.equal(8);
+        expect(penneWithSauce.getPrice()).to.equal(13);
+        expect(penneWithSauceAndCheese.getPrice()).to.equal(16);
+    });
+
+});
