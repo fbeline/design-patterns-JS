43c580d46c78938fa4643e3ff40a0c618b7b4e1d
Felipe Baravieira <fe.belineb@gmail.com>
2017-03-10 23:20:07 -0300

    + iterator es6

--- test/iterator-test.js
+++ test/iterator-test.js
@@ -1,18 +1,30 @@
 const expect = require('chai').expect;
 
 const Iterator = require('../src/behavioral/iterator/iterator');
-
+const Iterator6 = require('../src/behavioral/iterator/iterator_es6');
 
 describe('iterator tests', () => {
 
     it('sanity', () => {
-        var numbers = new Iterator([1,2,3]);
-
-        expect(numbers.next()).to.equal(1);
-        expect(numbers.next()).to.equal(2);
-        expect(numbers.next()).to.equal(3);
-        expect(numbers.hasNext()).to.false;
+        test(Iterator);
     });
 
 });
 
+
+describe('iterator es6 tests', () => {
+
+    it('sanity', () => {
+        test(Iterator6);
+    });
+
+});
+
+function test(Iterator) {
+    var numbers = new Iterator([1,2,3]);
+
+    expect(numbers.next()).to.equal(1);
+    expect(numbers.next()).to.equal(2);
+    expect(numbers.next()).to.equal(3);
+    expect(numbers.hasNext()).to.false;
+}
