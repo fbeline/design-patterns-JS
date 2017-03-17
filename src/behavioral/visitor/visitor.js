19a801aa37c8dbf97bb4633d9f775e66655659d2
Felipe Baravieira <fe.belineb@gmail.com>
2017-03-16 23:15:48 -0300

    + visitor

---
+++ src/behavioral/visitor/visitor.js
@@ -0,0 +1,28 @@
+function Operation() {}
+
+Operation.prototype.accept = function(visitor) {
+    return visitor.visit(this);
+}
+
+function Sum() {
+    this.val = " + ";
+}
+Sum.prototype = Object.create(Operation.prototype);
+
+function Min() {
+    this.val = " - ";
+}
+Min.prototype = Object.create(Operation.prototype);
+
+function Num(val) {
+    this.val = val;
+}
+Num.prototype = Object.create(Operation.prototype);
+
+
+function Printer() {}
+Printer.prototype.visit = function(operation) {
+    return operation.val;
+}
+
+module.exports = [Sum, Min, Num, Printer];
---
+++ test/visitor-test.js
@@ -0,0 +1,17 @@
+const expect = require('chai').expect;
+
+const [Sum, Min, Num, Printer] = require('../src/behavioral/visitor/visitor.js');
+
+
+describe('visitor tests', () => {
+    it('sanity', () => {
+        expression = [new Num(10), new Sum(), new Num(5), new Min(), new Num(3)];
+
+        result = "";
+        expression.forEach(el => {
+           result += el.accept(new Printer());
+        });
+
+        expect(result).to.equal("10 + 5 - 3");
+    });
+});
