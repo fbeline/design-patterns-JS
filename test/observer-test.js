b90d0727112f3a7aa1b63a7fb57dbaa95e028d31
Felipe Baravieira <fe.belineb@gmail.com>
2017-03-19 23:24:39 -0300

    +observer es6

--- test/observer-test.js
+++ test/observer-test.js
@@ -1,17 +1,29 @@
 const expect = require('chai').expect;
 const [Product, fees, proft] = require('../src/behavioral/observer/observer.js');
+const [Product6, fees6, proft6] = require('../src/behavioral/observer/observer_es6.js');
 
 
 describe('observer test', () => {
 
     it('sanity', () => {
-        var product = new Product();
-        product.register(fees);
-        product.register(proft);
-
-        product.setBasePrice(100);
-
-        expect(product.price).to.equal(240);
+        test(Product, fees, proft);
     });
 
 });
+
+describe('observer es6 test', () => {
+
+    it('sanity', () => {
+        test(Product6, new fees6(), new proft6());
+    });
+
+});
+
+
+function test(Product, fees, proft) {
+    var product = new Product();
+    product.register(fees);
+    product.register(proft);
+    product.setBasePrice(100);
+    expect(product.price).to.equal(240);
+}
