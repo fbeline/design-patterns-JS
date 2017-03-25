1961465b3a844da4928e971f5f833016ed86e667
Felipe Baravieira <fe.belineb@gmail.com>
2017-03-25 10:40:49 -0300

    +prototype

---
+++ src/creational/prototype/prototype.js
@@ -0,0 +1,11 @@
+function Sheep(name, weight) {
+    this.name = name;
+    this.weight = weight;
+}
+
+Sheep.prototype.clone = function() {
+    return new Sheep(this.name, this.weight);
+}
+
+
+module.exports = Sheep;
---
+++ test/prototype-test.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+const Sheep = require('../src/creational/prototype/prototype');
+
+describe('prototype test', () => {
+    it('sanity', () => {
+        var sheep = new Sheep('dolly', 10.3);
+        var dolly = sheep.clone();
+        expect(dolly.name).to.equal('dolly');
+    });
+});
