d227eb4982f318b985078b28d4b5d76f6935bc90
Felipe Baravieira <fe.belineb@gmail.com>
2017-03-26 20:32:28 -0300

    + chain of resp

---
+++ src/behavioral/chain-of-resp/chain-of-resp.js
@@ -0,0 +1,61 @@
+function ShoppingCart() {
+    this.products = [];
+
+    this.addProduct = function(p) {
+        this.products.push(p);
+    }
+}
+
+function Discount() {
+    this.calc = function(products) {
+        var ndiscount = new NumberDiscount();
+        var pdiscount = new PriceDiscount();
+        var none = new NoneDiscount();
+
+        ndiscount.setNext(pdiscount);
+        pdiscount.setNext(none);
+
+        return ndiscount.exec(products);
+    }
+}
+
+function NumberDiscount() {
+    this.next = null;
+    this.setNext = function(fn) {
+        this.next = fn;
+    }
+
+    this.exec = function(products) {
+        var result = 0;
+        if (products.length > 3)
+            result = 0.05;
+
+        return result + this.next.exec(products);
+    }
+}
+
+function PriceDiscount() {
+    this.next = null;
+    this.setNext = function(fn) {
+        this.next = fn;
+    }
+    this.exec = function(products) {
+        var result = 0;
+        var total = products.reduce(function(a, b) {
+            return a + b;
+        });
+
+        if (total >= 500)
+            result = 0.1;
+
+        return result + this.next.exec(products);
+    }
+}
+
+function NoneDiscount() {
+    this.exec = function() {
+        return 0;
+    }
+}
+
+module.exports = [ShoppingCart, Discount];
---
+++ test/chain-of-resp-test.js
@@ -0,0 +1,45 @@
+const expect = require('chai').expect;
+
+const [ShoppingCart, Discount]  = require('../src/behavioral/chain-of-resp/chain-of-resp');
+
+describe('chain of resp tests', () => {
+
+    it(' > $ 500', () => {
+        var discount = new Discount();
+
+        var sc = new ShoppingCart();
+        sc.addProduct(1000);
+
+        var resp = discount.calc(sc.products);
+
+        expect(resp).to.equal(0.1);
+    });
+
+    it('mote than 3 products', () => {
+        var discount = new Discount();
+
+        var sc = new ShoppingCart();
+        sc.addProduct(100);
+        sc.addProduct(100);
+        sc.addProduct(100);
+        sc.addProduct(100);
+
+        var resp = discount.calc(sc.products);
+
+        expect(resp).to.equal(0.05);
+    });
+
+    it('mote than 3 products and > $ 500 ', () => {
+        var discount = new Discount();
+
+        var sc = new ShoppingCart();
+        sc.addProduct(1000);
+        sc.addProduct(100);
+        sc.addProduct(100);
+        sc.addProduct(100);
+
+        var resp = discount.calc(sc.products);
+
+        expect(resp.toFixed(2)).to.equal('0.15');
+    });
+});
