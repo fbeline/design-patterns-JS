3d1b04acb4ef361475d7199008aad7860bf83c3b
Felipe Baravieira <fe.belineb@gmail.com>
2017-03-27 20:34:25 -0300

    +builder

---
+++ test/builder-test.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect;
+const RequestBuilder = require('../src/creational/builder/builder');
+
+describe('builder test', () => {
+    it('sanity', () => {
+        var requestBuilder = new RequestBuilder();
+        var request = requestBuilder
+            .forUrl('http://something/users')
+            .useMethod('GET')
+            .payload(null)
+            .build();
+
+        expect(request.method).to.equal('GET');
+    });
+});
