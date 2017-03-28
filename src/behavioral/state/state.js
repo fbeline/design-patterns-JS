2d4998988a6400cc6ca3a7e5d3fab48bb513dd1d
Felipe Baravieira <fe.belineb@gmail.com>
2017-03-27 23:06:02 -0300

    + state

---
+++ src/behavioral/state/state.js
@@ -0,0 +1,31 @@
+function Order() {
+    this.state = new WaintingForPayment();
+
+    this.nextState = function() {
+        this.state = this.state.next();
+    }
+}
+
+
+function WaintingForPayment() {
+    this.name = 'waitingForPayment';
+    this.next = function() {
+        return new Shipping();
+    }
+}
+
+function Shipping() {
+    this.name = 'shipping';
+    this.next = function() {
+        return new Delivered();
+    }
+}
+
+function Delivered() {
+    this.name = 'delivered';
+    this.next = function() {
+        return this;
+    }
+}
+
+module.exports = Order;
---
+++ test/state-test.js
@@ -0,0 +1,16 @@
+const expect = require('chai').expect;
+
+const Order = require('../src/behavioral/state/state');
+
+describe('state tests', () => {
+
+    it('sanity', () => {
+        var order = new Order();
+        expect(order.state.name).to.equal('waitingForPayment');
+        order.nextState();
+        expect(order.state.name).to.equal('shipping');
+        order.nextState();
+        expect(order.state.name).to.equal('delivered');
+    });
+
+});
