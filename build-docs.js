07f7cfdd7fd22e509aeabc70099c0184633a96f9
HyphnKnight <rafe.lepre@skedge.me>
2017-03-29 13:42:54 -0400

    add data types

--- build-docs.js
+++ build-docs.js
@@ -6,3 +6,25 @@ const SOURCE_DIR = './src';
 const TEST_DIR = './test';
 const DOCS_FILE = './docs.md';
 
+/* Data Storage */
+const sections = [];
+
+// A map of file paths to contents
+// e.g. { 'src/ex.js' : 'console.log("test")' }
+const files = new Map();
+
+
+/* Building Data Types  */
+const makeSection = ( name ) => ({
+    name,
+    path: `${SOURCE_DIR}/${name}`,
+    topics: [],
+});
+
+const makeTopic = ( sectionName ) => ( name ) => ({
+    name,
+    path: `${SOURCE_DIR}/${sectionName}/${name}`,
+    files: [],
+    tests: [],
+});
+
