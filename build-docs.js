a423fc38fb59964631b72f48c942e9efe23a7ea4
HyphnKnight <rafe.lepre@skedge.me>
2017-03-29 13:42:40 -0400

    add build-docs

---
+++ build-docs.js
@@ -0,0 +1,8 @@
+const nPath = require('path');
+const fs = require('fs-extra-promise');
+
+/* Path Constants */
+const SOURCE_DIR = './src';
+const TEST_DIR = './test';
+const DOCS_FILE = './docs.md';
+
