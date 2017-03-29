39b02f75c978781fbb9ba3d748126c6e0fc04f9d
Felipe Baravieira <fe.belineb@gmail.com>
2017-03-28 21:52:22 -0300

    + singleton

---
+++ test/singleton-test.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+const Person = require('../src/creational/singleton/singleton');
+
+describe('singleton test', () => {
+    it('sanity', () => {
+        var john = new Person();
+        var john2 = new Person();
+
+        expect(john).to.equal(john2);
+    });
+});
