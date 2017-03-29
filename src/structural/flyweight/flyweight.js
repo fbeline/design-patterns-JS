9d292c57e7f92d3de216ca3b5322ebaa89ec5663
Felipe Baravieira <fe.belineb@gmail.com>
2017-03-28 21:31:11 -0300

    + flyweight

---
+++ src/structural/flyweight/flyweight.js
@@ -0,0 +1,16 @@
+function Color(name) {
+    this.name = name;
+}
+
+var colorFactory = {
+    colors: {},
+    create: function(name) {
+        var color = this.colors[name];
+        if(color) return color;
+
+        this.colors[name] = new Color(name);
+        return this.colors[name];
+    }
+}
+
+module.exports = [colorFactory];
