63c5b29ec3360cd115314ff8bb21de41a083ce65
Felipe Baravieira <fe.belineb@gmail.com>
2017-03-30 23:23:41 -0300

    +template es6

---
+++ test/template_es6-test.js
@@ -0,0 +1,16 @@
+const expect = require('chai').expect;
+
+const [Tax1, Tax2] = require('../src/behavioral/template/template_es6');
+
+describe('template es6 tests', () => {
+
+    it('sanity', () => {
+        var tax1 = new Tax1();
+        var tax2 = new Tax2();
+
+        expect(tax1.calc(1000)).to.equal(1110);
+        expect(tax2.calc(1000)).to.equal(1210);
+        expect(tax2.calc(100)).to.equal(110);
+    });
+
+});
