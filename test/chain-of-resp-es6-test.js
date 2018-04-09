30f04fe13adfc0e098c07e2cd5ea35080d4a2a01
naor tedgi <naor2205@gmail.com>
2018-04-09 12:37:17 +0300

    spec in es6

--- test/chain-of-resp-es6-test.js
+++ test/chain-of-resp-es6-test.js
@@ -5,40 +5,40 @@ const [ShoppingCart, Discount]  = require('../src/behavioral/chain-of-resp/chain
 describe('chain of resp es6 tests', () => {
 
     it(' > $ 500', () => {
-        var discount = new Discount();
+        const discount = new Discount();
 
-        var sc = new ShoppingCart();
+        const sc = new ShoppingCart();
         sc.addProduct(1000);
 
-        var resp = discount.calc(sc.products);
+        let resp = discount.calc(sc.products);
 
         expect(resp).to.equal(0.1);
     });
 
     it('mote than 3 products', () => {
-        var discount = new Discount();
+        const discount = new Discount();
 
-        var sc = new ShoppingCart();
+        const sc = new ShoppingCart();
         sc.addProduct(100);
         sc.addProduct(100);
         sc.addProduct(100);
         sc.addProduct(100);
 
-        var resp = discount.calc(sc.products);
+        let resp = discount.calc(sc.products);
 
         expect(resp).to.equal(0.05);
     });
 
     it('mote than 3 products and > $ 500 ', () => {
-        var discount = new Discount();
+        let discount = new Discount();
 
-        var sc = new ShoppingCart();
+        let sc = new ShoppingCart();
         sc.addProduct(1000);
         sc.addProduct(100);
         sc.addProduct(100);
         sc.addProduct(100);
 
-        var resp = discount.calc(sc.products);
+        let resp = discount.calc(sc.products);
 
         expect(resp.toFixed(2)).to.equal('0.15');
     });
