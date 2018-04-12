0330d948cad1b2d6633ada6c49a1bc6728f4444f
naor tedgi <naor2205@gmail.com>
2018-04-12 08:53:18 +0300

    fix specs

--- test/flyweight_es6-test.js
+++ test/flyweight_es6-test.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 
-import colorFactory  from '../src/structural/flyweight/flyweight_es6';
+import  { colorFactory }  from '../src/structural/flyweight/flyweight_es6';
 
 describe('flyweight tests', () => {
 
